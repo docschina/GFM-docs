16dd1bc013fca3f70c9d780c130887cfa5147148
许云峰 <xuyunfeng.fe@bytedance.com>
2019-09-11 15:47:37 +0800

    style：github-markdown 的样式

--- .vuepress/components/Example.vue
+++ .vuepress/components/Example.vue
@@ -66,15 +66,29 @@
     mounted() {
       // console.log('$site', this.$site)
       // console.log('$page', this.$page)
-      this.$refs.demo.contentDocument.write(this.example.html)
+      const html = `<div class="markdown-body">${this.example.html}</div>\n<link href="/css/github-markdown.css" rel="stylesheet"></link>`
+      this.$refs.demo.contentDocument.write(html);
     }
   }
 </script>
 <style>
-    @media (min-width: 768px) {
+    @media (min-width: 480px) {
         table td {
             min-width: 200px;
             width: 33.3333%;
         }
+    }
+    .example-demo pre {
+        display: block;
+        padding: 9.5px;
+        margin: 0 0 10px;
+        font-size: 13px;
+        line-height: 1.42857143;
+        color: #333;
+        word-break: break-all;
+        word-wrap: break-word;
+        background-color: #f5f5f5;
+        border: 1px solid #ccc;
+        border-radius: 4px;
     }
 </style>
